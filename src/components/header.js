c358d4415aa06c308f7ce50a595888ca6a8a8b18
jahusv <125425367+jahusv@users.noreply.github.com>
2024-10-27 13:41:49 +0700

    Create header.js

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1 style="color:bisque">Main website/Главная страница</h1>
+      <a href=".../ru/ru.html">russion/русская</a>
+      <a href=".../eng/eng.html">english/аглийская</a>
+    </header>
+  );
+}
+
+export default Header;
