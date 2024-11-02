55c7151a8a93ad9d3458e4ce784b1e52767f7947
jahusv <125425367+jahusv@users.noreply.github.com>
2024-11-02 15:23:51 +0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header>
       <h1 style="color:bisque">Main website/Главная страница</h1>
-      <a href=".../ru/ru.html">russion/русская</a>
-      <a href=".../eng/eng.html">english/аглийская</a>
+      <a href="../ru/index.html">russion/русская</a>
+      <a href="../eng/index.html">english/аглийская</a>
     </header>
   );
 }
